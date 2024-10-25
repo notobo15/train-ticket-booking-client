7e9e63755f26e095042faf5c078d67f621c01832
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-10-25 23:51:00 +0700

    added: PopularTrain component

---
+++ components/PopularTrain/index.tsx
@@ -0,0 +1,199 @@
+import React from "react";
+
+export default function Index() {
+  return (
+    <section
+      className="grid grid-cols-[minmax(0,80rem)] justify-center pe-[5vw] ps-[5vw] md:pe-[6.25vw] md:ps-[6.25vw] py-500 gap-y-300 md:py-600 lg:py-800 lg:gap-y-500"
+      id="origin-from"
+    >
+      <h2 className="font-family-display-html text-size-150 font-weight-bolder leading-175 tracking-tight md:font-family-display-html md:text-size-200 md:font-weight-bolder md:leading-225 md:tracking-tight text-balance text-center undefined">
+        Popular Buses Connecting Albany
+      </h2>
+      <h3 className="font-family-display-html text-size-125 font-weight-bolder leading-150 tracking-tight md:font-family-display-html md:text-size-150 md:font-weight-bolder md:leading-175 md:tracking-tight text-balance text-center undefined">
+        Buses Leaving from Albany
+      </h3>
+      <ul className="grid gap-x-200 sm:grid-cols-2 lg:grid-cols-3 *:flex *:min-w-0 *:justify-between *:gap-x-500 *:py-200">
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-albany-rensselaer/r/dred9s-dp611f"
+              >
+                Bus from Albany to Rensselaer
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-albany-buffalo/r/dred9s-dpxgvp"
+              >
+                Bus Albany - Buffalo
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-albany-boston/r/dred9s-drt2yz"
+              >
+                Albany - Boston
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-albany-montreal/r/dred9s-f25dvk"
+              >
+                Bus from Albany to Montreal
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-albany-miami/r/dred9s-dhwfxh"
+              >
+                Bus Albany - Miami
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-albany-philadelphia/r/dred9s-dr4e39"
+              >
+                Albany - Philadelphia
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-albany-syracuse/r/dred9s-dr9ugh"
+              >
+                Bus from Albany to Syracuse
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-albany-binghamton/r/dred9s-dr6p2r"
+              >
+                Bus Albany - Binghamton
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-albany-newburgh/r/dred9s-dr7k71"
+              >
+                Albany - Newburgh
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-albany-oneonta/r/dred9s-drd9sq"
+              >
+                Bus from Albany to Oneonta
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-albany-ithaca/r/dred9s-dr997q"
+              >
+                Bus Albany - Ithaca
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-albany-rochester/r/dred9s-dr8v9b"
+              >
+                Albany - Rochester
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-albany-lake-placid/r/dred9s-drg7sj"
+              >
+                Bus from Albany to Lake Placid
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-albany-kingston/r/dred9s-dr7qs0"
+              >
+                Bus Albany - Kingston
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-albany-hartford/r/dred9s-drkmd4"
+              >
+                Albany - Hartford
+              </a>
+            </span>
+          </span>
+        </li>
+      </ul>
+    </section>
+  );
+}
