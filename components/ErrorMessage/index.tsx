e865493d94e52967c81bf6411904553c26b3d816
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-10-28 23:15:38 +0700

    added: ErrorMessage component

---
+++ components/ErrorMessage/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { MdErrorOutline } from "react-icons/md";
+import styles from "./ErrrorMessage.module.scss";
+export default function index({ message }: { message: string }) {
+  return (
+    <div className={styles.error}>
+      <div className={styles.errorWrapper}>
+        <span className={styles.errorIcon}>
+          <MdErrorOutline size={24} />
+        </span>
+        <span className={styles.errorMessage}>{message}</span>
+      </div>
+    </div>
+  );
+}
