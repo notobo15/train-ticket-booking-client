4e800cc7e2238d324c634f354e652d7d4158d3f8
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-10-30 23:35:34 +0700

    added: PopularRoute component

---
+++ components/PopularRoute/index.tsx
@@ -0,0 +1,196 @@
+import React from "react";
+
+export default function Index() {
+  return (
+    <section
+      className="grid grid-cols-[minmax(0,80rem)] justify-center pe-[5vw] ps-[5vw] md:pe-[6.25vw] md:ps-[6.25vw] py-500 gap-y-300 md:py-600 lg:py-800 lg:gap-y-500"
+      id="popular-routes-country"
+    >
+      <h2 className="font-family-display-html text-size-150 font-weight-bolder leading-175 tracking-tight md:font-family-display-html md:text-size-200 md:font-weight-bolder md:leading-225 md:tracking-tight text-balance text-center undefined">
+        Popular routes in this country
+      </h2>
+      <ul className="grid gap-x-200 sm:grid-cols-2 lg:grid-cols-3 *:flex *:min-w-0 *:justify-between *:gap-x-500 *:py-200">
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-fort-lauderdale-orlando/r/dhxh87-djn4k5"
+              >
+                Bus from Fort Lauderdale to Orlando
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-orlando-fort-lauderdale/r/djn4k5-dhxh87"
+              >
+                Bus Orlando - Fort Lauderdale
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-dallas-houston/r/9vg4mp-9vk1mf"
+              >
+                Dallas - Houston
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-las-vegas-los-angeles/r/9qqjgc-9q5ctr"
+              >
+                Bus from Las Vegas to Los Angeles
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-albany-new-york/r/dred9s-dr5reg"
+              >
+                Bus Albany - New York
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-new-york-niagara-falls/r/dr5reg-dpxv0g"
+              >
+                New York - Niagara Falls
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-new-york-buffalo/r/dr5reg-dpxgvp"
+              >
+                Bus from New York to Buffalo
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-new-york-atlantic-city/r/dr5reg-dqgpfq"
+              >
+                Bus New York - Atlantic City
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-seattle-leavenworth/r/c23nb6-c26nwp"
+              >
+                Seattle - Leavenworth
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-tampa-miami/r/dhvqgx-dhwfxh"
+              >
+                Bus from Tampa to Miami
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-los-angeles-san-francisco/r/9q5ctr-9q8yyk"
+              >
+                Bus Los Angeles - San Francisco
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-new-york-los-angeles/r/dr5reg-9q5ctr"
+              >
+                New York - Los Angeles
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-austin-dallas/r/9v6kpv-9vg4mp"
+              >
+                Bus from Austin to Dallas
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-miami-tampa/r/dhwfxh-dhvqgx"
+              >
+                Bus Miami - Tampa
+              </a>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="max-w-full font-weight-bold text-size-87 leading-100 inline-flex items-center gap-x-025 min-w-0">
+            <span className="truncate">
+              <a
+                className="focus-visible:outline-none focus-visible:ring aria-disabled:cursor-default text-color-scheme-interactive-link-600 hover:text-color-scheme-interactive-link-500 active:text-color-scheme-interactive-link-600 hover:underline visited:text-color-scheme-literal-purple-600"
+                href="https://www.busbud.com/en/bus-buffalo-new-york/r/dpxgvp-dr5reg"
+              >
+                Buffalo - New York
+              </a>
+            </span>
+          </span>
+        </li>
+      </ul>
+    </section>
+  );
+}
