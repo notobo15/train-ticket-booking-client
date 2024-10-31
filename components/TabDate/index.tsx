9b86a6f69b5f64407f6b5f38479567093eacc136
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-10-31 23:38:21 +0700

    added: TabDate component

---
+++ components/TabDate/index.tsx
@@ -0,0 +1,217 @@
+import React from "react";
+
+export default function Index() {
+  return (
+    <section className="grid grid-cols-[minmax(0,80rem)] justify-center pe-[5vw] ps-[5vw] md:pe-[6.25vw] md:ps-[6.25vw] py-500 gap-y-300 md:py-600 lg:py-800 lg:gap-y-500 !mb-0 !pb-0">
+      <div className="-sm:mx-300 -md:mx-[5%] relative -mx-200 lg:mx-0">
+        <div className="lg:-py-300 flex overflow-x-auto bg-color-canvas-primary px-200 sm:px-300 md:px-[5%] lg:px-300">
+          <div
+            className="xs:min-w-[calc(100%/3.5)] relative flex min-w-[calc(100%/2.5)] justify-center px-150 py-200 hover:cursor-pointer sm:min-w-[calc(100%/4.5)] md:min-w-[auto] md:flex-1 lg:py-300 [&.active]:rounded-t-md [&.active]:bg-[color:var(--col-bg-canvas-sec)] [&.active]:before:absolute [&.active]:before:-left-300 [&.active]:before:bottom-0 [&.active]:before:h-150 [&.active]:before:w-300 [&.active]:before:rounded-br-md [&.active]:before:shadow-[0.75rem_0_0_0_var(--col-bg-canvas-sec)] [&.active]:before:content-[''] [&.active]:after:absolute [&.active]:after:-right-300 [&.active]:after:bottom-0 [&.active]:after:h-150 [&.active]:after:w-300 [&.active]:after:rounded-bl-md [&.active]:after:shadow-[-0.75rem_0_0_0_var(--col-bg-canvas-sec)] [&.active]:after:content-['']"
+            data-price-calendar-item="true"
+            data-price-calendar-has-departures="false"
+            data-href="/en/bus-schedules-results/dred9s/dr6p2r?outbound_date=2024-10-15&adults=1"
+          >
+            <div className="flex flex-col items-center">
+              <span className="max-w-full font-weight-bold text-size-75 leading-100 inline-block">
+                Today
+              </span>
+              <span className="max-w-full font-weight-bold text-size-87 leading-100 sm:text-size-100 sm:leading-125 inline-block sm:inline-block">
+                29 Tue
+              </span>
+              <svg
+                fill="none"
+                height={24}
+                width={24}
+                aria-hidden="true"
+                className="mt-100 text-icon-color-primary xl:mt-200"
+              >
+                <use href="https://www.busbud.com/pubweb-assets/horizon/Icons/1.12.5/MagnifyingGlass/md.svg#root" />
+              </svg>
+            </div>
+          </div>
+          <div
+            className="xs:min-w-[calc(100%/3.5)] relative flex min-w-[calc(100%/2.5)] justify-center px-150 py-200 hover:cursor-pointer sm:min-w-[calc(100%/4.5)] md:min-w-[auto] md:flex-1 lg:py-300 [&.active]:rounded-t-md [&.active]:bg-[color:var(--col-bg-canvas-sec)] [&.active]:before:absolute [&.active]:before:-left-300 [&.active]:before:bottom-0 [&.active]:before:h-150 [&.active]:before:w-300 [&.active]:before:rounded-br-md [&.active]:before:shadow-[0.75rem_0_0_0_var(--col-bg-canvas-sec)] [&.active]:before:content-[''] [&.active]:after:absolute [&.active]:after:-right-300 [&.active]:after:bottom-0 [&.active]:after:h-150 [&.active]:after:w-300 [&.active]:after:rounded-bl-md [&.active]:after:shadow-[-0.75rem_0_0_0_var(--col-bg-canvas-sec)] [&.active]:after:content-[''] active"
+            data-price-calendar-item="true"
+            data-price-calendar-has-departures="true"
+            data-href="/en/bus-schedules-results/dred9s/dr6p2r?outbound_date=2024-10-16&adults=1"
+          >
+            <div className="flex flex-col items-center">
+              <span className="max-w-full font-weight-bold text-size-75 leading-100 inline-block">
+                Tomorrow
+              </span>
+              <span className="max-w-full font-weight-bold text-size-87 leading-100 sm:text-size-100 sm:leading-125 inline-block sm:inline-block">
+                Oct 16
+              </span>
+              <div className="inline-flex max-w-full items-center gap-x-025 rounded-pill ps-075 pe-075 py-050 backdrop-blur-lg bg-color-scheme-neutral-100 text-color-primary mt-100 xl:mt-200">
+                <span className="font-weight-bold ps-050 pe-050 py-000 text-size-75 leading-100">
+                  <div className="flex flex-wrap items-center justify-center whitespace-nowrap">
+                    <span className="max-w-full text-size-56 uppercase leading-75 tracking-wider inline-block text-color-tertiary">
+                      from<span>&nbsp;</span>
+                    </span>
+                    <span className="max-w-full font-weight-bold text-size-62 uppercase leading-75 tracking-wider inline-block">
+                      $90
+                    </span>
+                  </div>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div
+            className="xs:min-w-[calc(100%/3.5)] relative flex min-w-[calc(100%/2.5)] justify-center px-150 py-200 hover:cursor-pointer sm:min-w-[calc(100%/4.5)] md:min-w-[auto] md:flex-1 lg:py-300 [&.active]:rounded-t-md [&.active]:bg-[color:var(--col-bg-canvas-sec)] [&.active]:before:absolute [&.active]:before:-left-300 [&.active]:before:bottom-0 [&.active]:before:h-150 [&.active]:before:w-300 [&.active]:before:rounded-br-md [&.active]:before:shadow-[0.75rem_0_0_0_var(--col-bg-canvas-sec)] [&.active]:before:content-[''] [&.active]:after:absolute [&.active]:after:-right-300 [&.active]:after:bottom-0 [&.active]:after:h-150 [&.active]:after:w-300 [&.active]:after:rounded-bl-md [&.active]:after:shadow-[-0.75rem_0_0_0_var(--col-bg-canvas-sec)] [&.active]:after:content-['']"
+            data-price-calendar-item="true"
+            data-price-calendar-has-departures="false"
+            data-href="/en/bus-schedules-results/dred9s/dr6p2r?outbound_date=2024-10-17&adults=1"
+          >
+            <div className="flex flex-col items-center">
+              <span className="max-w-full font-weight-bold text-size-75 leading-100 inline-block">
+                Thu
+              </span>
+              <span className="max-w-full font-weight-bold text-size-87 leading-100 sm:text-size-100 sm:leading-125 inline-block sm:inline-block">
+                Oct 17
+              </span>
+              <svg
+                fill="none"
+                height={24}
+                width={24}
+                aria-hidden="true"
+                className="mt-100 text-icon-color-primary xl:mt-200"
+              >
+                <use href="https://www.busbud.com/pubweb-assets/horizon/Icons/1.12.5/MagnifyingGlass/md.svg#root" />
+              </svg>
+            </div>
+          </div>
+          <div
+            className="xs:min-w-[calc(100%/3.5)] relative flex min-w-[calc(100%/2.5)] justify-center px-150 py-200 hover:cursor-pointer sm:min-w-[calc(100%/4.5)] md:min-w-[auto] md:flex-1 lg:py-300 [&.active]:rounded-t-md [&.active]:bg-[color:var(--col-bg-canvas-sec)] [&.active]:before:absolute [&.active]:before:-left-300 [&.active]:before:bottom-0 [&.active]:before:h-150 [&.active]:before:w-300 [&.active]:before:rounded-br-md [&.active]:before:shadow-[0.75rem_0_0_0_var(--col-bg-canvas-sec)] [&.active]:before:content-[''] [&.active]:after:absolute [&.active]:after:-right-300 [&.active]:after:bottom-0 [&.active]:after:h-150 [&.active]:after:w-300 [&.active]:after:rounded-bl-md [&.active]:after:shadow-[-0.75rem_0_0_0_var(--col-bg-canvas-sec)] [&.active]:after:content-['']"
+            data-price-calendar-item="true"
+            data-price-calendar-has-departures="true"
+            data-href="/en/bus-schedules-results/dred9s/dr6p2r?outbound_date=2024-10-18&adults=1"
+          >
+            <div className="flex flex-col items-center">
+              <span className="max-w-full font-weight-bold text-size-75 leading-100 inline-block">
+                Fri
+              </span>
+              <span className="max-w-full font-weight-bold text-size-87 leading-100 sm:text-size-100 sm:leading-125 inline-block sm:inline-block">
+                Oct 18
+              </span>
+              <div className="inline-flex max-w-full items-center gap-x-025 rounded-pill ps-075 pe-075 py-050 backdrop-blur-lg bg-color-scheme-neutral-100 text-color-primary mt-100 xl:mt-200">
+                <span className="font-weight-bold ps-050 pe-050 py-000 text-size-75 leading-100">
+                  <div className="flex flex-wrap items-center justify-center whitespace-nowrap">
+                    <span className="max-w-full text-size-56 uppercase leading-75 tracking-wider inline-block text-color-tertiary">
+                      from<span>&nbsp;</span>
+                    </span>
+                    <span className="max-w-full font-weight-bold text-size-62 uppercase leading-75 tracking-wider inline-block">
+                      $46
+                    </span>
+                  </div>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div
+            className="xs:min-w-[calc(100%/3.5)] relative flex min-w-[calc(100%/2.5)] justify-center px-150 py-200 hover:cursor-pointer sm:min-w-[calc(100%/4.5)] md:min-w-[auto] md:flex-1 lg:py-300 [&.active]:rounded-t-md [&.active]:bg-[color:var(--col-bg-canvas-sec)] [&.active]:before:absolute [&.active]:before:-left-300 [&.active]:before:bottom-0 [&.active]:before:h-150 [&.active]:before:w-300 [&.active]:before:rounded-br-md [&.active]:before:shadow-[0.75rem_0_0_0_var(--col-bg-canvas-sec)] [&.active]:before:content-[''] [&.active]:after:absolute [&.active]:after:-right-300 [&.active]:after:bottom-0 [&.active]:after:h-150 [&.active]:after:w-300 [&.active]:after:rounded-bl-md [&.active]:after:shadow-[-0.75rem_0_0_0_var(--col-bg-canvas-sec)] [&.active]:after:content-['']"
+            data-price-calendar-item="true"
+            data-price-calendar-has-departures="true"
+            data-href="/en/bus-schedules-results/dred9s/dr6p2r?outbound_date=2024-10-19&adults=1"
+          >
+            <div className="flex flex-col items-center">
+              <span className="max-w-full font-weight-bold text-size-75 leading-100 inline-block">
+                Sat
+              </span>
+              <span className="max-w-full font-weight-bold text-size-87 leading-100 sm:text-size-100 sm:leading-125 inline-block sm:inline-block">
+                Oct 19
+              </span>
+              <div className="inline-flex max-w-full items-center gap-x-025 rounded-pill ps-075 pe-075 py-050 backdrop-blur-lg bg-color-scheme-literal-coral-500 text-color-primary-inverse mt-100 xl:mt-200">
+                <span className="font-weight-bold ps-050 pe-050 py-000 text-size-75 leading-100">
+                  <div className="flex flex-wrap items-center justify-center whitespace-nowrap">
+                    <span className="max-w-full text-size-56 uppercase leading-75 tracking-wider inline-block text-color-primary-inverse">
+                      from<span>&nbsp;</span>
+                    </span>
+                    <span className="max-w-full font-weight-bold text-size-62 uppercase leading-75 tracking-wider inline-block">
+                      $44
+                    </span>
+                  </div>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div
+            className="xs:min-w-[calc(100%/3.5)] relative flex min-w-[calc(100%/2.5)] justify-center px-150 py-200 hover:cursor-pointer sm:min-w-[calc(100%/4.5)] md:min-w-[auto] md:flex-1 lg:py-300 [&.active]:rounded-t-md [&.active]:bg-[color:var(--col-bg-canvas-sec)] [&.active]:before:absolute [&.active]:before:-left-300 [&.active]:before:bottom-0 [&.active]:before:h-150 [&.active]:before:w-300 [&.active]:before:rounded-br-md [&.active]:before:shadow-[0.75rem_0_0_0_var(--col-bg-canvas-sec)] [&.active]:before:content-[''] [&.active]:after:absolute [&.active]:after:-right-300 [&.active]:after:bottom-0 [&.active]:after:h-150 [&.active]:after:w-300 [&.active]:after:rounded-bl-md [&.active]:after:shadow-[-0.75rem_0_0_0_var(--col-bg-canvas-sec)] [&.active]:after:content-['']"
+            data-price-calendar-item="true"
+            data-price-calendar-has-departures="true"
+            data-href="/en/bus-schedules-results/dred9s/dr6p2r?outbound_date=2024-10-20&adults=1"
+          >
+            <div className="flex flex-col items-center">
+              <span className="max-w-full font-weight-bold text-size-75 leading-100 inline-block">
+                Sun
+              </span>
+              <span className="max-w-full font-weight-bold text-size-87 leading-100 sm:text-size-100 sm:leading-125 inline-block sm:inline-block">
+                Oct 20
+              </span>
+              <div className="inline-flex max-w-full items-center gap-x-025 rounded-pill ps-075 pe-075 py-050 backdrop-blur-lg bg-color-scheme-neutral-100 text-color-primary mt-100 xl:mt-200">
+                <span className="font-weight-bold ps-050 pe-050 py-000 text-size-75 leading-100">
+                  <div className="flex flex-wrap items-center justify-center whitespace-nowrap">
+                    <span className="max-w-full text-size-56 uppercase leading-75 tracking-wider inline-block text-color-tertiary">
+                      from<span>&nbsp;</span>
+                    </span>
+                    <span className="max-w-full font-weight-bold text-size-62 uppercase leading-75 tracking-wider inline-block">
+                      $46
+                    </span>
+                  </div>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div
+            className="xs:min-w-[calc(100%/3.5)] relative flex min-w-[calc(100%/2.5)] justify-center px-150 py-200 hover:cursor-pointer sm:min-w-[calc(100%/4.5)] md:min-w-[auto] md:flex-1 lg:py-300 [&.active]:rounded-t-md [&.active]:bg-[color:var(--col-bg-canvas-sec)] [&.active]:before:absolute [&.active]:before:-left-300 [&.active]:before:bottom-0 [&.active]:before:h-150 [&.active]:before:w-300 [&.active]:before:rounded-br-md [&.active]:before:shadow-[0.75rem_0_0_0_var(--col-bg-canvas-sec)] [&.active]:before:content-[''] [&.active]:after:absolute [&.active]:after:-right-300 [&.active]:after:bottom-0 [&.active]:after:h-150 [&.active]:after:w-300 [&.active]:after:rounded-bl-md [&.active]:after:shadow-[-0.75rem_0_0_0_var(--col-bg-canvas-sec)] [&.active]:after:content-['']"
+            data-price-calendar-item="true"
+            data-price-calendar-has-departures="false"
+            data-href="/en/bus-schedules-results/dred9s/dr6p2r?outbound_date=2024-10-21&adults=1"
+          >
+            <div className="flex flex-col items-center">
+              <span className="max-w-full font-weight-bold text-size-75 leading-100 inline-block">
+                Mon
+              </span>
+              <span className="max-w-full font-weight-bold text-size-87 leading-100 sm:text-size-100 sm:leading-125 inline-block sm:inline-block">
+                Oct 21
+              </span>
+              <svg
+                fill="none"
+                height={24}
+                width={24}
+                aria-hidden="true"
+                className="mt-100 text-icon-color-primary xl:mt-200"
+              >
+                <use href="https://www.busbud.com/pubweb-assets/horizon/Icons/1.12.5/MagnifyingGlass/md.svg#root" />
+              </svg>
+            </div>
+          </div>
+          <div
+            className="xs:min-w-[calc(100%/3.5)] relative flex min-w-[calc(100%/2.5)] justify-center px-150 py-200 hover:cursor-pointer sm:min-w-[calc(100%/4.5)] md:min-w-[auto] md:flex-1 lg:py-300 [&.active]:rounded-t-md [&.active]:bg-[color:var(--col-bg-canvas-sec)] [&.active]:before:absolute [&.active]:before:-left-300 [&.active]:before:bottom-0 [&.active]:before:h-150 [&.active]:before:w-300 [&.active]:before:rounded-br-md [&.active]:before:shadow-[0.75rem_0_0_0_var(--col-bg-canvas-sec)] [&.active]:before:content-[''] [&.active]:after:absolute [&.active]:after:-right-300 [&.active]:after:bottom-0 [&.active]:after:h-150 [&.active]:after:w-300 [&.active]:after:rounded-bl-md [&.active]:after:shadow-[-0.75rem_0_0_0_var(--col-bg-canvas-sec)] [&.active]:after:content-['']"
+            data-price-calendar-item="true"
+            data-price-calendar-has-departures="false"
+            data-href="/en/bus-schedules-results/dred9s/dr6p2r?outbound_date=2024-10-22&adults=1"
+          >
+            <div className="flex flex-col items-center">
+              <span className="max-w-full font-weight-bold text-size-75 leading-100 inline-block">
+                Tue
+              </span>
+              <span className="max-w-full font-weight-bold text-size-87 leading-100 sm:text-size-100 sm:leading-125 inline-block sm:inline-block">
+                Oct 22
+              </span>
+              <svg
+                fill="none"
+                height={24}
+                width={24}
+                aria-hidden="true"
+                className="mt-100 text-icon-color-primary xl:mt-200"
+              >
+                <use href="https://www.busbud.com/pubweb-assets/horizon/Icons/1.12.5/MagnifyingGlass/md.svg#root" />
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div className="absolute bottom-0 right-0 top-0 w-400 bg-gradient-to-l from-color-static-white-canvas md:hidden"></div>
+      </div>
+    </section>
+  );
+}
