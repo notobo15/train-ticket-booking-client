496ac89662114f987291330dedde0a39733ff751
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-11-10 23:01:38 +0700

    added: props component

--- components/FloatingLabelInput/index.tsx
+++ components/FloatingLabelInput/index.tsx
@@ -1,45 +1,59 @@
 "use client";
-import React from "react";
+import React, { ChangeEvent } from "react";
 
 export default function FloatingLabelInput({
   id,
   label,
   type = "text",
+  name,
   value,
   onChange,
   className = "",
   icon,
+  error,
+  disabled,
+  description,
 }: {
   id: string;
   label: string;
   type?: string;
+  name?: string;
   value: string;
-  onChange: (_: string) => void;
-  className: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void; // Updated type
+  className?: string;
   icon?: React.ReactNode;
+  error?: string | boolean;
+  disabled?: boolean;
+  description?: string;
 }) {
   return (
     <div className={`relative ${className}`}>
-      <div className="flex items-center">
-        <input
-          type={type}
-          id={id}
-          value={value}
-          onChange={(e) => onChange(e.target.value)}
-          className="block px-2.5 pb-2.5 pt-4 w-full text-base text-gray-900 bg-transparent rounded-md border-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-        />
-        {icon && (
-          <div className="absolute right-3 top-1/2 transform -translate-y-1/2 pointer-events-none">
-            {icon}
-          </div>
-        )}
-      </div>
+      <input
+        type={type}
+        id={id}
+        name={name}
+        value={value}
+        disabled={disabled}
+        onChange={onChange}
+        className={`block px-2.5 pb-2.5 pt-4 w-full text-base text-color-primary rounded-md border-2 ${
+          error
+            ? "border-[rgb(213,26,45)] bg-[rgb(255,238,239)]"
+            : "border-gray-300 bg-transparent focus:border-blue-600"
+        } appearance-none focus:outline-none focus:ring-0 peer`}
+        placeholder=" "
+      />
+
+      {icon && <div className="absolute right-3 top-1/2 transform -translate-y-1/2 pointer-events-none">{icon}</div>}
+
       <label
         htmlFor={id}
-        className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 left-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4"
+        className={`absolute text-sm ${
+          error ? "text-[rgb(252,44,58)]" : "text-gray-500 dark:text-gray-400"
+        } duration-300 transform -translate-y-4 scale-75 top-2 left-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4`}
       >
         {label}
       </label>
+      {description && <p className="absolute mt-1 ms-2 text-xs text-gray-500">{description}</p>}
     </div>
   );
 }
