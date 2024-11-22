5617dbb7f8184639d9c7d32e41192d9404d17b9a
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-11-22 23:22:11 +0700

    added: global styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,45 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        "canvas-primary": "var(--col-bg-canvas-prim)",
+        "negative-400": "var(--col-negative-400)",
+        "link-600": "var(--col-link-600)",
+        transparent: "var(--col-transparent)",
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+
+      spacing: {
+        "100": "var(--d-100)",
+        "150": "var(--d-150)",
+        "200": "var(--d-200)",
+        "250": "var(--d-250)",
+        "300": "var(--d-300)",
+        "350": "var(--d-350)",
+      },
+      boxShadow: {
+        none: "0 0 #0000",
+        sm: "var(--s-sm)",
+        md: "var(--s-md)",
+        raised: "var(--s-el-raised)",
+      },
+      letterSpacing: {
+        tight: "var(--ls-tight)",
+        tighter: "var(--ls-tighter)",
+        wide: "var(--ls-wide)",
+        wider: "var(--ls-wider)",
+      },
+      lineHeight: {
+        reset: "var(--lh-reset)",
+      },
+      gridTemplateColumns: {
+        "custom-1": "25% 1fr minmax(min-content, 12.5%)",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      padding: ["responsive"], // Đảm bảo các lớp padding có thể phản hồi theo kích thước màn hình
     },
   },
   plugins: [],
