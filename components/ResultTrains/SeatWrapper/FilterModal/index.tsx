bc5de369a82eb71e76c01cc001b84e4446e60129
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-11-23 01:37:20 +0700

    added: FilterModal component in result search train

---
+++ components/ResultTrains/SeatWrapper/FilterModal/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "./FilterModal.module.scss";
+import clsx from "clsx";
+import { IoClose } from "react-icons/io5";
+import { useAppDispatch } from "@/redux/hooks";
+import { setIsOpenModalFilter } from "@/redux/features/searchSlice";
+import FilterTime from "../../FilterTime";
+export default function Index({ open }: { open: boolean }) {
+  const dispatch = useAppDispatch();
+  const handleClose = () => {
+    dispatch(setIsOpenModalFilter(false));
+  };
+  return (
+    <div className={styles.modal}>
+      <div className={clsx(styles.bakcdrop, { [styles.active]: open })} onClick={handleClose}></div>
+      <div className={styles.container}>
+        <div className={clsx(styles.wrapper, { [styles.close]: !open })}>
+          <div className={styles.header}>
+            <div className={styles.headerLeft}>
+              <button onClick={handleClose}>
+                <IoClose size={32} />
+              </button>
+              <h6>Filters</h6>
+            </div>
+            <div className={styles.headerRight}>
+              <span>Reset all</span>
+            </div>
+          </div>
+          <div className={styles.main}>
+            <FilterTime />
+          </div>
+          <div className={styles.footer}>
+            <div className={styles.result}>
+              <span>39 results found</span>
+            </div>
+            <button className={styles.btnDone}>
+              <span>Done</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
