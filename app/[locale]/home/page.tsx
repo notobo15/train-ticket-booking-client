f90624f97a70dde265de70f93eff21af7c12233d
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-11-23 01:32:55 +0700

    added: home page

---
+++ app/[locale]/home/page.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "@/styles/styles.css";
+import Header from "@/components/Header";
+import Banner from "@/components/Banner";
+import SubBanner from "@/components/SubBanner";
+import SearchForm from "@/components/SearchForm";
+import Footer from "@/components/Footer";
+import TagLink from "@/components/TagLink";
+import PopularRoute from "@/components/PopularRoute";
+import About from "@/components/About";
+import TopTravel from "@/components/TopTravel";
+import PopularTrain from "@/components/PopularTrain";
+export default function Index() {
+  return (
+    <>
+      <header className="[background:var(--header-gradient)]">
+        <div className="grid grid-cols-[minmax(0,80rem)] justify-center pe-[5vw] ps-[5vw] md:pe-[6.25vw] md:ps-[6.25vw] relative overflow-x-clip pb-500 md:pb-600 lg:pb-800 md:bg-[1.5rem_0%_center]">
+          <Header />
+          <div></div>
+          <Banner />
+          <div className="relative flex flex-col gap-150 py-300">
+            <SearchForm />
+          </div>
+          <SubBanner />
+        </div>
+      </header>
+      <>
+        <main
+          className="focus-visible:outline-none focus-visible:before:ring focus-visible:relative focus-visible:before:absolute focus-visible:before:inset-050 focus-visible:before:pointer-events-none focus-visible:before:rounded-sm"
+          id="main"
+          tabIndex={-1}
+        >
+          {/* <TabDate /> */}
+          <TagLink />
+
+          <About />
+          <TopTravel />
+          <PopularTrain />
+          <PopularRoute />
+        </main>
+        <Footer />
+      </>
+    </>
+  );
+}
