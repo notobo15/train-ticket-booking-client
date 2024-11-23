184aa036ba420d7b643b42a3e6c3ba59ea240358
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-11-23 14:37:29 +0700

    added: CarriageList component

---
+++ app/[locale]/search/CarriageList/index.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React, { useRef } from "react";
+import Carriage from "../Carrriage";
+import styles from "./CarriageList.module.scss";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/scrollbar";
+import { Navigation, Scrollbar } from "swiper/modules";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
+
+export default function CarriageList({ carriages }: { carriages: Carriage[] }) {
+  const prevRef = useRef<HTMLDivElement>(null);
+  const nextRef = useRef<HTMLDivElement>(null);
+  return (
+    <div className={styles.carriageContainer}>
+      <div className={styles.btnPrev} ref={prevRef}>
+        <FaAngleLeft size={24} />
+      </div>
+      <Swiper
+        spaceBetween={10}
+        slidesPerView={3}
+        pagination={{ clickable: true }}
+        scrollbar={{ enabled: true, draggable: true }}
+        modules={[Navigation, Scrollbar]}
+        navigation={{
+          enabled: true,
+          prevEl: prevRef.current,
+          nextEl: nextRef.current,
+        }}
+        breakpoints={{
+          640: { slidesPerView: 2 },
+          768: { slidesPerView: 3 },
+          1024: { slidesPerView: 4 },
+        }}
+      >
+        {carriages.map((carriage, index) => (
+          <SwiperSlide key={index}>
+            {/* {index === 0 && <div className={styles.trainHead}></div>} */}
+            <Carriage
+              {...carriage}
+              // seats={carriage.seats}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className={styles.btnNext} ref={nextRef}>
+        <FaAngleRight size={24} />
+      </div>
+    </div>
+  );
+}
